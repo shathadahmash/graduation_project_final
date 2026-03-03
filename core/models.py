bb942bf9889e78a52a9e24903277c9b4252ab685
shatha <shathadahmash@gmail.com>
2026-03-03 11:20:10 +0300

    adding the CID to the user table

--- core/models.py
+++ core/models.py
@@ -254,6 +254,12 @@ class StudentProgress(models.Model):
 # 3. نموذج المستخدم المخصص
 # ==============================================================================
 class User(AbstractUser):
+    CID = models.CharField(
+        max_length=20,
+        unique=True,
+        null=True,
+        blank=True
+    )
     phone = models.CharField(max_length=20, blank=True, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     gender = models.CharField(
