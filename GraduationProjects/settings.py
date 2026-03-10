79e7adfe241f75212b8af91f4cb467a59eea2d2f
shatha <shathadahmash@gmail.com>
2026-03-10 13:54:27 +0300

    correcting the admin

--- GraduationProjects/settings.py
+++ GraduationProjects/settings.py
@@ -121,12 +121,14 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '3307',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
+
+USE_TZ = False
 
 # -------------------------
 # AUTHENTICATION
